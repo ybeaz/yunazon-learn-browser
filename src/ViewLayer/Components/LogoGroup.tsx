bd60562cfbea4708410561be2b606f9ddb5ccdc4
Roman <rcheskidov@atlassian.com>
2021-05-24 14:50:08 -0700

    perf(devops): prepare for direct analytics collection VI

--- src/ViewLayer/Components/LogoGroup.tsx
+++ src/ViewLayer/Components/LogoGroup.tsx
@@ -1,20 +1,23 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
-import { getStringifyToAzClass } from '../../Shared/getStringifyToAzClass'
+import { handleEvents } from '../Hooks/handleEvents'
 
 export const LogoGroup: React.FunctionComponent<any> = (): JSX.Element => {
-  const trackObj = { type: 'click', name: 'logo clicked' }
-  const azClass = getStringifyToAzClass({ obj: trackObj })
+  const saveAnalyticsEvent = {
+    type: 'SAVE_ANALYTICS_EVENT',
+    data: { type: 'click', name: 'logo clicked' },
+  }
 
   return (
     <Link
-      className={`LogoGroup ${azClass}`}
+      className='LogoGroup'
       to={{
         pathname: `/home`,
       }}
+      onClick={() => handleEvents({}, saveAnalyticsEvent)}
     >
-      <div className={`__div `}>
+      <div className='__div'>
         <img
           className='_img'
           src='https://yourails.com/images/logoYouRailsV13.png'
