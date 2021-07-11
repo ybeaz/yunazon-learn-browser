f6787c1075fa018964766c97dcd62f515969f688
Roman <rcheskidov@atlassian.com>
2021-07-10 20:36:11 -0700

    perf(devops): add proto AuthUser component

---
+++ src/ViewLayer/Components/AuthUser.tsx
@@ -0,0 +1,8 @@
+import React, { useState, useEffect, useRef, ReactElement } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
+export const AuthUser: React.FunctionComponent<any> = (
+  props: any
+): JSX.Element => {
+  return <div className='AuthUser'>AuthUser</div>
+}
