8fa54b18ed679b1f2893c0e2fd2cc3d8fa58051d
admin <admin@3440-mbp.gateway.sonic.net>
2021-09-10 16:54:37 -0700

    perf(devops): setup Select component II

---
+++ src/ViewLayer/Components/Select.tsx
@@ -0,0 +1,85 @@
+import React, { useState } from 'react'
+import { nanoid } from 'nanoid'
+
+import { handleEvents } from '../Hooks/handleEvents'
+
+interface PropsEvent {
+  typeEvent: string
+  data?: any
+}
+
+interface Option {
+  text?: string
+  value?: string
+  defaultSelected: boolean
+  selected: boolean
+}
+
+interface SelectArgs {
+  size: number
+  options: Option[]
+}
+
+export const Select: React.FunctionComponent<SelectArgs> = (
+  props: SelectArgs
+): JSX.Element => {
+  const { size, options } = props
+
+  const [sizeState, setSizeState] = useState(0)
+
+  const getOptions = (optionsIn2: Option[]): React.ReactElement[] => {
+    return optionsIn2.map((option: Option) => {
+      const { text, value, defaultSelected, selected } = option
+      const nanoID = nanoid()
+      const optionProps = {
+        key: nanoID,
+        className: '_option',
+        value,
+        defaultSelected,
+        selected,
+      }
+      return <option {...optionProps}>{text}</option>
+    })
+  }
+
+  const handleComponentEvents = (event: any, propsEvent: PropsEvent): void => {
+    const { typeEvent, data } = propsEvent
+
+    const output = {
+      SELECT_ON_MOUSE_DOWN: () => options.length > size && setSizeState(size),
+      SELECT_ON_CHANGE: () => {
+        setSizeState(0)
+        handleEvents(event, { typeEvent: 'SELECT_ON_CHANGE' })
+      },
+      SELECT_ON_BLUR: () => setSizeState(0),
+    }
+
+    output[typeEvent]
+      ? output[typeEvent]()
+      : console.info('Select handleComponentEvents [error]', 'strange type', {
+          typeEvent,
+          data,
+        })
+  }
+
+  return (
+    <div className='Select'>
+      <select
+        className='__selectTag'
+        name='select_component'
+        size={sizeState}
+        onMouseDown={(event: any) =>
+          handleComponentEvents(event, { typeEvent: 'SELECT_ON_MOUSE_DOWN' })
+        }
+        onChange={(event: any) =>
+          handleComponentEvents(event, { typeEvent: 'SELECT_ON_CHANGE' })
+        }
+        onBlur={(event: any) =>
+          handleComponentEvents(event, { typeEvent: 'SELECT_ON_BLUR' })
+        }
+      >
+        {getOptions(options)}
+      </select>
+    </div>
+  )
+}
