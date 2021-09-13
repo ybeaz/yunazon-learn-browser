8d0f835d57697690902759fa9300796695ccca6a
admin <admin@3440-mbp.gateway.sonic.net>
2021-09-12 22:19:10 -0700

    perf(devops): add component Select with multiple feature II

--- src/ViewLayer/Components/Select.tsx
+++ src/ViewLayer/Components/Select.tsx
@@ -22,6 +22,10 @@ interface SelectArgs {
   sizeOnBlur: number
 }
 
+interface GetOptionsNextInterface {
+  (options: Option[], arrSelected: string[]): Option[]
+}
+
 export const Select: React.FunctionComponent<SelectArgs> = (
   props: SelectArgs
 ): JSX.Element => {
@@ -47,11 +51,7 @@ export const Select: React.FunctionComponent<SelectArgs> = (
     })
   }
 
-  interface GetOptionsNext {
-    (options: Option[], arrSelected: string[]): Option[]
-  }
-
-  const getOptionsNext: GetOptionsNext = (options2, arrSelected) => {
+  const getOptionsNext: GetOptionsNextInterface = (options2, arrSelected) => {
     let output = options2.map((option: Option) => {
       const { value, selected } = option
       let selectedNext = false
@@ -83,69 +83,70 @@ export const Select: React.FunctionComponent<SelectArgs> = (
     return output
   }
 
+  const SELECT_ON_MOUSE_DOWN = (event: any): void => {
+    options.length > size && setSizeState(size)
+
+    const optionsStateSelected = optionsState.filter(
+      item => item.selected === true
+    )
+
+    if (
+      !onBlurState &&
+      optionsStateSelected.length === 1 &&
+      event.target.value === optionsStateSelected[0].value
+    ) {
+      const optionsStateNext = options.map((item, i) =>
+        i === 0 ? { ...item, selected: true } : item
+      )
+      setOptionsState(optionsStateNext)
+    } else {
+      onBlurState && setOptionsState(optionsState2)
+      setOnBlurState(false)
+    }
+  }
+
+  const SELECT_ON_CHANGE = (event: any): void => {
+    setSizeState(size)
+
+    const arrSelected = event.target.selectedOptions
+      ? Array.from(event.target.selectedOptions, (option: any) => option.value)
+      : [event.target.value]
+
+    const optionsStateNext = getOptionsNext(optionsState, arrSelected)
+
+    setOptionsState(optionsStateNext)
+
+    const dataSelected = optionsStateNext
+      .filter((item, i) => i !== 0 && item.selected === true)
+      .map(item => item.value)
+
+    handleEvents(event, {
+      typeEvent: 'SELECT_ON_CHANGE',
+      data: dataSelected,
+    })
+  }
+
+  const SELECT_ON_BLUR = (): void => {
+    const optionsSelected = optionsState.filter(item => item.selected === true)
+    const sizeSelected = optionsSelected.length ? optionsSelected.length : 1
+
+    setOnBlurState(true)
+    setSizeState(sizeSelected)
+    setOptionsState2(optionsState)
+    setOptionsState(optionsSelected)
+  }
+
   const handleComponentEvents = (event: any, propsEvent: PropsEvent): void => {
     const { typeEvent, data } = propsEvent
 
     const output = {
-      SELECT_ON_MOUSE_DOWN: () => {
-        options.length > size && setSizeState(size)
-
-        const optionsStateSelected = optionsState.filter(
-          item => item.selected === true
-        )
-
-        if (
-          !onBlurState &&
-          optionsStateSelected.length === 1 &&
-          event.target.value === optionsStateSelected[0].value
-        ) {
-          const optionsStateNext = options.map((item, i) =>
-            i === 0 ? { ...item, selected: true } : item
-          )
-          setOptionsState(optionsStateNext)
-        } else {
-          onBlurState && setOptionsState(optionsState2)
-          setOnBlurState(false)
-        }
-      },
-      SELECT_ON_CHANGE: () => {
-        setSizeState(size)
-
-        const arrSelected = event.target.selectedOptions
-          ? Array.from(
-              event.target.selectedOptions,
-              (option: any) => option.value
-            )
-          : [event.target.value]
-
-        const optionsStateNext = getOptionsNext(optionsState, arrSelected)
-
-        setOptionsState(optionsStateNext)
-
-        const dataSelected = optionsStateNext
-          .filter((item, i) => i !== 0 && item.selected === true)
-          .map(item => item.value)
-
-        handleEvents(event, {
-          typeEvent: 'SELECT_ON_CHANGE',
-          data: dataSelected,
-        })
-      },
-      SELECT_ON_BLUR: () => {
-        const optionsSelected = optionsState.filter(
-          item => item.selected === true
-        )
-        const sizeSelected = optionsSelected.length ? optionsSelected.length : 1
-
-        setOnBlurState(true)
-        setSizeState(sizeSelected)
-        setOptionsState2(optionsState)
-        setOptionsState(optionsSelected)
-      },
+      SELECT_ON_MOUSE_DOWN,
+      SELECT_ON_CHANGE,
+      SELECT_ON_BLUR,
     }
 
     output[typeEvent]
-      ? output[typeEvent]()
+      ? output[typeEvent](event)
       : console.log('Select handleComponentEvents [error]', 'strange type', {
           typeEvent,
           data,
