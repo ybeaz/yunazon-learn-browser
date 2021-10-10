4c5e7c8876928555bb125d87a2a08e814c55c659
admin <admin@3440-MacBook-Pro.local>
2021-10-10 20:02:27 +0500

    perf(devops): make theme dynamic XXVI

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -1,8 +1,27 @@
 import 'styled-components'
 
+interface Dictionary {
+  Light: string
+  Dark: string
+}
 // and extend them!
 declare module 'styled-components' {
   export interface DefaultTheme {
+    colors: {
+      colorFirstDarker2: Dictionary
+      colorFirstDarker: Dictionary
+      colorFirst: Dictionary
+
+      colorSecondDacker: Dictionary
+      colorSecond: Dictionary
+      colorSecondLighter2: Dictionary
+      colorSecondLighter3: Dictionary
+      colorSecondLighter4: Dictionary
+
+      colorActive: Dictionary
+      colorActiveDacker: Dictionary
+      colorBoxes: Dictionary
+    }
     color: string
     background: string
   }
--- src/ViewLayer/Styles/GlobalTheme.tsx
+++ src/ViewLayer/Styles/GlobalTheme.tsx
@@ -28,12 +28,64 @@ export const GlobalTheme: React.FunctionComponent<GlobalThemeArgs> = (
   } = useSelector((store2: IRootStore) => store2)
 
   const globalTheme = {
+    colors: {
+      colorFirstDarker2: {
+        Light: '',
+        Dark: 'rgba(255, 255, 255, 0.8)',
+      },
+      colorFirstDarker: {
+        Light: '',
+        Dark: 'rgba(255, 255, 255, 0.9)',
+      },
+      colorFirst: {
+        Light: '',
+        Dark: 'rgb(255, 255, 255)',
+      },
+
+      colorSecondDacker: {
+        Light: '',
+        Dark: 'rgb(24, 24, 24)',
+      },
+      colorSecond: {
+        Light: '',
+        Dark: 'rgb(32, 32, 32)',
+      },
+      colorSecondLighter2: {
+        Light: '',
+        Dark: 'rgb(50, 50, 50)',
+      },
+      colorSecondLighter3: {
+        Light: '',
+        Dark: 'rgb(55, 55, 55)',
+      },
+      colorSecondLighter4: {
+        Light: '',
+        Dark: 'rgb(111, 111, 111)',
+      },
+
+      colorActive: {
+        Light: 'yellow',
+        Dark: 'rgb(56, 94, 182)',
+      },
+      colorActiveDacker: {
+        Light: '',
+        Dark: 'rgb(22, 37, 61)',
+      },
+      colorBoxes: {
+        Light: '',
+        Dark: 'rgb(62, 166, 255)',
+      },
+    },
+
     background: theme === 'Light' ? 'lightgreen' : 'lightblue',
     color: theme === 'Light' ? 'black' : 'grey',
     colorActive: 'red', // theme === 'Light' ? 'blue' : 'red',
   }
 
   const GlobalStyleTheme = createGlobalStyle`
+    .CatalogSEP .__titleScreen {
+      color: ${props2 => props2.theme.colors.colorActive[theme]};
+    }
     body {
       color: ${props2 => props2.theme.color};
       background: ${props2 => props2.theme.background};
