5be12e907e17d9e400049e4f86e889c92fea532b
APKAWY7PMABJWOMVRXDJ <admin@3440-mbp.gateway.sonic.net>
2022-03-31 15:13:29 -0700

    perf(devops): add AuthAwsCognitoLink component III

--- src/FeatureFlags/flags.ts
+++ src/FeatureFlags/flags.ts
@@ -2,5 +2,9 @@ import { getDetectedEnv } from '../Shared/getDetectedEnv'
 
 const envType: string = getDetectedEnv()
 
+/** @description Flag to toggle display of the Cognito signin option in the Header */
 export const isAwsCognitoAuth = (envTypeIn = envType) => true
+/** @description Flag to toggle option to save analytics to the proprietor server, service statee TODO */
 export const isGetingSavedAnanlyticsEvent = (envTypeIn = envType) => false
+/** @description Flag template */
+export const isTemplate = (envTypeIn = envType) => false
