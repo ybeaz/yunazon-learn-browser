ef90fe02a7a249df1636fde540ce4c8d87bdfd58
Roman Ches <contact@yourails.com>
2023-12-22 23:42:46 -0800

    perf(develop): sign up a new customer while certificate granting, XI

--- src/DataLayer/handlers/SET_MODAL_FRAMES.ts
+++ src/DataLayer/handlers/SET_MODAL_FRAMES.ts
@@ -5,6 +5,5 @@ import { actionSync } from '../../DataLayer/index.action'
 const { dispatch } = store
 
 export const SET_MODAL_FRAMES: ActionEventType = (event, data) => {
-  console.info('SET_MODAL_FRAMES handler [8]', { data })
   dispatch(actionSync.SET_MODAL_FRAMES(data))
 }
