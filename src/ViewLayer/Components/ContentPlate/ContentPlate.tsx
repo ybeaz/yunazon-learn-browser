945508d6cbd0285a800156242e9ce2fce6b41d9b
Roman Ches <contact@yourails.com>
2024-05-22 20:27:57 -0700

    perf(develop): develop combination tags and module representation, XXXI

--- src/ViewLayer/Components/ContentPlate/ContentPlate.tsx
+++ src/ViewLayer/Components/ContentPlate/ContentPlate.tsx
@@ -131,7 +131,7 @@ const ContentPlateComponent: ContentPlateComponentType = (
   }
 
   return (
-    <div className={getClasses('ContentPlate')} key={moduleID} id={moduleID}>
+    <div className={getClasses('ContentPlate')} key={moduleID}>
       <CONTENT_ASSIGNED_COMPONENT {...propsOut.contentComponentProps[contentComponentName]}>
         {isCompleted ? (
           <div className='_isCompleted'>
