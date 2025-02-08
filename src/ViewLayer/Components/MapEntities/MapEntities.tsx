c1f82e7809beee45f323b3657fbd6eb57499279a
Roman Ches <contact@yourails.com>
2025-02-07 19:33:39 -0800

    perf(feat): add engaging features, IX

---
+++ src/ViewLayer/Components/MapEntities/MapEntities.tsx
@@ -0,0 +1,77 @@
+import React, { Attributes, FunctionComponent } from 'react'
+
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  Marker,
+  ComposableMapProps,
+} from 'react-simple-maps'
+
+import { withPropsYrl, withStoreStateSelectedYrl } from 'yourails_common'
+import { getClasses } from 'yourails_common'
+import {
+  MapEntitiesComponentPropsType,
+  MapEntitiesPropsType,
+  MapEntitiesPropsOutType,
+  MapEntitiesComponentType,
+  MapEntitiesType,
+} from './MapEntitiesTypes'
+
+const ComposableMapFC = ({ children }: { children: any }) =>
+  React.createElement(ComposableMap as FunctionComponent<ComposableMapProps>, {
+    projection: 'geoAzimuthalEqualArea',
+    projectionConfig: {
+      rotate: [58, 20, 0],
+      scale: 600,
+    },
+    children,
+  })
+
+/**
+ * @description Component to render MapEntities
+ * @import import { MapEntities, MapEntitiesPropsType, MapEntitiesPropsOutType, MapEntitiesType } 
+             from '../Components/MapEntities/MapEntities'
+ */
+const MapEntitiesComponent: MapEntitiesComponentType = (props: MapEntitiesComponentPropsType) => {
+  const { classAdded, geoUrl, markers } = props
+
+  const propsOut: MapEntitiesPropsOutType = {}
+
+  return (
+    <div className={getClasses('MapEntities', classAdded)}>
+      <ComposableMapFC>
+        {/* @ts-ignore */}
+        <Geographies geography={geoUrl}>
+          {({ geographies }) =>
+            geographies.map(geo => (
+              // @ts-ignore
+              <Geography key={geo.rsmKey} geography={geo} fill='#EAEAEC' stroke='#D6D6DA' />
+            ))
+          }
+        </Geographies>
+        {markers.map(({ name, coordinates, markerOffset }) => (
+          // @ts-ignore
+          <Marker key={name} coordinates={coordinates}>
+            <text
+              textAnchor='middle'
+              y={markerOffset}
+              style={{ fontFamily: 'system-ui', fill: '#5D5A6D' }}
+            >
+              {name}
+            </text>
+          </Marker>
+        ))}
+      </ComposableMapFC>
+    </div>
+  )
+}
+
+const storeStateSliceProps: string[] = []
+const MapEntities: MapEntitiesType = withStoreStateSelectedYrl(
+  storeStateSliceProps,
+  React.memo(MapEntitiesComponent)
+)
+
+export type { MapEntitiesPropsType, MapEntitiesType }
+export { MapEntities }
--- src/ViewLayer/Components/QuestionScores/QuestionScores.tsx
+++ src/ViewLayer/Components/QuestionScores/QuestionScores.tsx
@@ -14,6 +14,7 @@ import { handleEvents as handleEventsIn } from '../../../DataLayer/index.handleE
 import { getScenarioDict } from './getScenarioDict'
 import { FormInputNames } from '../FormInputNames/FormInputNames'
 import { withStoreStateSelectedYrl, withPropsYrl, ButtonYrl } from 'yourails_common'
+import { MapEntities, MapEntitiesPropsType } from '../MapEntities/MapEntities'
 
 import {
   GetScenarioDictPropsType,
@@ -202,6 +203,11 @@ const QuestionScoresComponent: QuestionScoresComponentType = (
     { markerOffset: 15, name: 'Lima', coordinates: [-77.0428, -12.0464] },
   ]
 
+  const mapEntitiesProps: MapEntitiesPropsType = {
+    geoUrl,
+    markers,
+  }
+
   console.info('QuestionScores [128]', { 'scenario.message': scenario.message })
 
   return (
@@ -215,43 +221,7 @@ const QuestionScoresComponent: QuestionScoresComponentType = (
         </div>
       </div>
 
-      <ComposableMap
-        projection='geoAzimuthalEqualArea'
-        projectionConfig={{
-          rotate: [58, 20, 0],
-          scale: 400,
-        }}
-      >
-        <Geographies geography={geoUrl}>
-          {({ geographies }) =>
-            geographies.map(geo => (
-              <Geography key={geo.rsmKey} geography={geo} fill='#EAEAEC' stroke='#D6D6DA' />
-            ))
-          }
-        </Geographies>
-        {markers.map(({ name, coordinates, markerOffset }) => (
-          <Marker key={name} coordinates={coordinates}>
-            <g
-              fill='none'
-              stroke='#FF5533'
-              strokeWidth='2'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              transform='translate(-12, -24)'
-            >
-              <circle cx='12' cy='10' r='3' />
-              <path d='M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z' />
-            </g>
-            <text
-              textAnchor='middle'
-              y={markerOffset}
-              style={{ fontFamily: 'system-ui', fill: '#5D5A6D' }}
-            >
-              {name}
-            </text>
-          </Marker>
-        ))}
-      </ComposableMap>
+      <MapEntities {...mapEntitiesProps} />
 
       <div className='_buttons'>
         <Tooltip className='_tooltip' title={buttonNextTaskTooltipText}>
